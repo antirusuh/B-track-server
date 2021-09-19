3bfc9b6bb1cc4c0d13e27b98c0e2a943f0783fc1
mmuhdar <mmuhdar17@gmail.com>
2021-09-19 14:00:43 +0700

    add category routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const budgetRoutes = require("./budgetRoutes");
 const transactionRoutes = require("./transactionRoutes");
 const userRoutes = require("./userRoutes");
+const categoryRoutes = require("../routes/categoryRoutes");
 const errorHandler = require("../middlewares/errorHandler");
 
 router.use("/", userRoutes);
 
 router.use("/budgets", budgetRoutes);
+
+router.use("/categories", categoryRoutes);
 
 router.use("/transactions", transactionRoutes);
 
